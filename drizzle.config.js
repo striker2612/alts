0838354722c1f59ba73a4dd04aef598030990776
striker2612 <nayanicharantej@gmail.com>
2024-10-17 11:13:24 +0530

    first commit

---
+++ drizzle.config.js
@@ -0,0 +1,9 @@
+/** @type { import("drizzle-kit").Config } */
+export default {
+    schema: "./utils/schema.tsx",
+    dialect: 'postgresql',
+    dbCredentials: {
+      url: 'Add Your Own URL',
+    }
+  };
+  
